4237cbefc8201ef5c10bcf49e451e083d58efa8f
cb0806151 <cb0806151@otc.edu>
2020-12-29 17:57:40 -0600

    get basic coin transfer working

--- index.mjs
+++ index.mjs
@@ -17,27 +17,42 @@ const N = 10;
     const fmt = (x) => stdlib.formatCurrency(x, 4);
     const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
 
+    const bet = Math.floor(Math.random() * 10);
+
     await Promise.all([
         backend.Auctioneer(ctcAuctioneer, {
             getParams: () => ({
-                betPrice: 5,
+                betPrice: stdlib.parseCurrency(5),
                 deadline: 10,
                 potAddr: accPot,
+                potAmount: stdlib.parseCurrency(5)
             }),
         }),
-    ].concat(
-        accAttendee_arr.map((accAttendee, i) => {
-          const ctcAttendee = accAttendee.attach(backend, ctcInfo);
-          const bet = Math.floor(Math.random() * 10);
-
-          return backend.Attendee(ctcAttendee, {
-            getBet: (() => bet),
+        backend.Attendee(accAttendee_arr[0].attach(backend, ctcInfo), {
+          getBet: (() => stdlib.parseCurrency(5)),
             AttendeeWas: ((AttendeeAddr) => {
               if ( stdlib.addressEq(AttendeeAddr, accAttendee) ) {
                 console.log(`${AttendeeAddr} bet: ${bet}`);
-              } } )}); 
-        } )
-      ));
+              }}) 
+        })
+    ]
+    // .concat(
+    //     accAttendee_arr.map((accAttendee, i) => {
+    //       const ctcAttendee = accAttendee.attach(backend, ctcInfo);
+    //       const bet = Math.floor(Math.random() * 10);
+
+    //       return backend.Attendee(ctcAttendee, {
+    //         postBet: (() => bet),
+    //         AttendeeWas: ((AttendeeAddr) => {
+    //           if ( stdlib.addressEq(AttendeeAddr, accAttendee) ) {
+    //             console.log(`${AttendeeAddr} bet: ${bet}`);
+    //           } } )}); 
+    //     } )
+    //   )
+      );
+
+    let total = await getBalance(accAttendee_arr[0]);
+    console.log(total)
 
     const potTotal = await getBalance(accPot);
     console.log(`The final price of the pot is ${potTotal}`);
--- index.rsh
+++ index.rsh
@@ -1,26 +1,37 @@
 'reach 0.1';
 
 
-export const main = 
+export const main =
     Reach.App(
         {},
         [['Auctioneer',
         { getParams: Fun([], Object({ betPrice: UInt,
                                         deadline: UInt,
-                                        potAddr: Address})) }],
-        ['class', 'Attendee',
+                                        potAddr: Address,
+                                        potAmount: UInt})) }],
+        ['Attendee',
         { getBet: Fun([], UInt),
             AttendeeWas: Fun([Address], Null),
         } ],
         ],
         (Auctioneer, Attendee) => {
             Auctioneer.only(() => {
-                const { betPrice, deadline, potAddr } =
+                const { betPrice, deadline, potAddr, potAmount } =
                   declassify(interact.getParams());
             });
-            Auctioneer.publish(betPrice, deadline, potAddr);
-            
-            transfer(balance()).to(potAddr);
+            Auctioneer.publish(betPrice, deadline, potAddr, potAmount);
+            commit();
+
+            Auctioneer.pay(potAmount);
+            commit();
+
+            Attendee.only(() => {
+                const bet = declassify(interact.getBet());
+            });
+            Attendee.publish(bet).pay(bet);
+
+            const winner = potAddr;
+            transfer(balance()).to(Attendee);
             commit();
         }
     );
