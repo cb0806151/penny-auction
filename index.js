7a36dbf3656ff3c6610bef425405e389eebe5ad0
cb0806151 <cb0806151@otc.edu>
2021-01-09 12:38:09 -0600

    start work on alternative mjs file for interactive version

---
+++ index.js
@@ -0,0 +1,7 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+import { ask, yesno, done } from '@reach-sh/stdlib/ask.mjs';
+
+(async () => {
+    const stdlib = await loadStdlib();
+})();
