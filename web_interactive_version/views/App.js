3658ffc1883b59994752e07df61864bfc5e9c7a9
cb0806151 <cb0806151@otc.edu>
2021-01-22 19:11:42 -0600

    get an idea as to how gradually building the application would work

---
+++ web_interactive_version/views/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const exports = {};
+
+exports.Wrapper = class extends React.Component {
+    render() {
+        const {content} = this.props;
+        return (
+            <div className="App">
+                <header className="App-header" id="root">
+                    <h1>Penny Auction</h1>
+                    {content}
+                </header>
+            </div>
+        );
+    }
+}
+
+exports.ConnectAccount = class extends React.Component {
+    render() {
+      return (
+        <div>
+          Please wait while we connect to your account.
+          If this takes more than a few seconds, there may be something wrong.
+        </div>
+      )
+    }
+  }
+
+export default exports;
